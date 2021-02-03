39e5b722fd10acb4c1fe21a7d990aae91136c09a
Yau <1173383184@qq.com>
2021-02-04 01:54:50 +0800

    ie life

--- src/views/Lifes.vue
+++ src/views/Lifes.vue
@@ -105,6 +105,26 @@ import fancybox from "@fancyapps/fancybox";
 						that.col = 2;
 					}
 				}).trigger("resize");
+				function IEVersion() {
+				  var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串		
+				var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; 
+				//判断是否IE<11浏览器								
+				var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //判断是否IE的Edge浏览器
+				var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+
+				if (isIE) {
+				$('.fancybox').removeAttr("data-fancybox");
+				} else if (isEdge) {
+				return 1;//edge
+				} else if (isIE11) {
+				$('.fancybox').removeAttr("data-fancybox");
+				return 1; //IE11
+				} else {
+				return -1;//不是ie浏览器
+				}
+				
+				}
+				IEVersion();
 				$('.fancybox').fancybox();
 				
 				
